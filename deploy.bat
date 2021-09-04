a88d4589b26ad4f227c5c97904d89b79629458e1
ineedbots <ineedbots@outlook.com>
2021-09-04 16:08:17 -0600

    Do dir

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 xcopy pt6_bot_warfare\raw\maps raw\maps\ /Y /I /E /H /C
 xcopy pt6_bot_warfare\raw\scripts raw\scripts\ /Y /I /E /H /C
-xcopy pt6_bot_warfare\main\dedicated.cfg main\ /Y /I /H /C
+xcopy pt6_bot_warfare\main\ main\ /Y /I /E /H /C
+xcopy pt6_bot_warfare\raw\bots.txt raw\ /Y /I /H /C
